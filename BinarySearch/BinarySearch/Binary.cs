1530748ceaf678f8c028d7544703b1d60ce0ee33
Rajesh kumar <rajeshbabu656656@gmail.com>
2022-12-11 11:03:19 +0530

    Adding 56 and then adding 30 and 70

---
+++ BinarySearch/BinarySearch/Binary.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    public class Binary<T> where T : IComparable<T>
+    {
+        public T NodeData
+        {
+            get; set;
+        }
+        public Binary<T> leftTree { get; set; }
+        public Binary<T> rightTree { get; set; }
+
+        public Binary(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.rightTree = null;
+            this.leftTree = null;
+
+        }
+        int leftCount = 0, rightCount = 0;
+        bool result = false;
+        public void Add(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if ((currentNodeValue.CompareTo(item)) > 0) // "A".CompareTo("a")
+            {
+                if (this.leftTree == null)
+                {
+                    this.leftTree = new Binary<T>(item);
+                }
+                else
+                    this.leftTree.Add(item);
+
+            }
+            else
+            {
+                if (this.rightTree == null)
+                {
+                    this.rightTree = new Binary<T>(item);
+                }
+                else
+                    this.rightTree.Add(item);
+            }
+        }
+        public void Display()
+        {
+            if (this.leftTree != null)
+            {
+                this.leftCount++;
+                this.leftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.rightTree != null)
+            {
+                this.rightCount++;
+                this.rightTree.Display();
+            }
+        }
+    }
+}
---
+++ BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace BinarySearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n************* WELCOME TO BINARY SEARCH TREE(BST) ******************");
+
+            Binary<int> binary = new Binary<int>(56);
+            binary.Add(30);
+            binary.Add(70);
+            binary.Display();
+            Console.ReadKey();
+        }
+    }
+}
